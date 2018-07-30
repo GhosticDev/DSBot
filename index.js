e9e076b9795982e557af2fdf9f2f48f886f3af9c
GhosticDev <ghosticman@gmail.com>
2018-07-30 03:47:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,27 +5,41 @@ const client = new Discord.Client();
 client.prefix = config.prefix;
 
 client.on("ready", () => {
-	console.log("Bot on with " + client.users.size + " users and " + client.guilds.size + " servers!");
-	client.user.setActivity(`${client.users.size} users!`, {type: 'Watching'});
+  console.log("Bot iniciado!\n\nUsers: " + client.users.size + "\nServidores: " + client.guilds.size);
+  client.user.setActivity(`${client.users.size} users`, {type: "Watching"});
 });
 
 client.on("message", async message => {
   let msg = message.content.toLowerCase();
-	if (message.author.bot) return undefined;
-  let user = message.author;
+  if (message.author.bot) return undefined;
 
-	if (message.content.indexOf(config.prefix) !== 0) return;
-	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
+  if (message.content.indexOf(client.prefix) !== 0) return;
+  const args = message.content.slice(client.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
 
   try {
     let commands = require(`./commands/${command}.js`);
     commands.run(client, message, args);
   } catch (e) {
-    utils.error(client, e, message.author.username);
-  } finally {
-  }
+    console.log(e);
+  } finally {}
 
 });
 
-client.login(process.env.TOKEN);
+client.on("guildMemberAdd", async member => {
+  client.channels.get('471615619016425503').send(`New member: **${member.user.username}** (${member.guild.name})`);
+});
+
+client.on("guildMemberRemove", async member => {
+  client.channels.get('471615619016425503').send(`Bye member: **${member.user.username}** (${member.guild.name})`);
+});
+
+client.on("guildCreate", async guild => {
+  client.channels.get('471615619016425503').send(`New server: **${guild.name}** (Owner: ${guild.owner.user.username})(Members: ${guild.memberCount})`);
+});
+
+client.on("guildDelete", async guild => {
+  client.channels.get('471615619016425503').send(`Bye server: **${guild.name}**`);
+});
+
+client.login(config.token);
