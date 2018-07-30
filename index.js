5c2cf2ec6a876569af22a7f01d800a64575918ac
GhosticDev <ghosticman@gmail.com>
2018-07-30 02:38:44 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -9,4 +9,23 @@ client.on("ready", () => {
 	client.user.setActivity(`${client.users.size} users!`, {type: 'Watching'});
 });
 
+client.on("message", async message => {
+  let msg = message.content.toLowerCase();
+	if (message.author.bot) return undefined;
+  let user = message.author;
+
+	if (message.content.indexOf(config.prefix) !== 0) return;
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+  try {
+    let commands = require(`./commands/${command}.js`);
+    commands.run(client, message, args);
+  } catch (e) {
+    utils.error(client, e, message.author.username);
+  } finally {
+  }
+
+});
+
 client.login(process.env.TOKEN);
